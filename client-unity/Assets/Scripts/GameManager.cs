ca05005dc00bfe757b471b7d34d476160eafc76f
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2025-02-19 13:06:52 -0500

    update server url

--- client-unity/Assets/Scripts/GameManager.cs
+++ client-unity/Assets/Scripts/GameManager.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    const string SERVER_URL = "http://52.54.111.197:3000";
-    const string MODULE_NAME = "blackholio-beau";
+    const string SERVER_URL = "http://127.0.0.1:3000";
+    const string MODULE_NAME = "blackholio";
 
     public static event Action OnConnected;
     public static event Action OnSubscriptionApplied;
